a0b932380aaeb498b5aaf5e9f3c91937abaf82e4
TexasBearKat <texasbearkaat@gmail.com>
2024-11-05 20:59:36 -0600

    Update imagecreate.py

--- imagehandling/imagecreate.py
+++ imagehandling/imagecreate.py
@@ -1,3 +1,9 @@
-import random as r
+import numpy as np
+from PIL import Image
 
+def create_image(name, height, width):
+    pixels = np.random.randint(0, 255, (height, width, 3))
+    pixels = np.array(pixels, dtype=np.uint8)
+    image = Image.fromarray(pixels)
+    image.save(f"{name}.png")
 
